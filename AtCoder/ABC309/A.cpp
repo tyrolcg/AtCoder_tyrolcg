e1ce0b3f01579ce004cacd446e96c9e469ceadf0
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-07-12 23:24:42 +0900

    A DONE

---
+++ AtCoder/ABC309/A.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int a,b;
+    cin >> a >> b;
+    if(a%3!=0 && b - a == 1)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
