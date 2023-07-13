bb48ff12647fc2a93bb6b47ba46005a12f5b64a3
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-07-13 16:47:01 +0900

    A DONE

---
+++ AtCoder/ABC308/A.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    vector<int> s(8);
+    bool flag = true;
+    for(int i = 0;i<8;i++){
+        cin >> s[i];
+        if(i>0 && s[i-1] > s[i]){
+            flag = false;
+            break;
+        }
+        if(s[i]<100||675<s[i]){
+            flag = false;
+            break;
+        }
+        if(s[i]%25!=0){
+            flag = false;
+            break;
+        }
+        
+    }
+    if(flag){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+    
+}
