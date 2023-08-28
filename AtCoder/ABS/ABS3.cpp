42de28571ba36e02af62793124097668707e957f
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-08-28 21:00:11 +0900

    3 done

---
+++ AtCoder/ABS/ABS3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int ans = 0;
+    for(int i = 0;i<3;i++){
+        if(s[i]=='1'){
+            ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
