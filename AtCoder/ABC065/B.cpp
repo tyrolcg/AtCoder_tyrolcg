3a1f194868dc81c3c3d426b7a070e60e19b6c2e5
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-10-09 11:49:08 +0900

    B done

---
+++ AtCoder/ABC065/B.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0;i<n;i++){
+        cin>>a[i];
+    }
+    int next = 1;
+    int ans = 0;
+    for(int i =0;i<n;i++){
+        if(next==2){
+            cout << ans;
+            return 0;
+        }
+        next = a[next-1];
+        ans++;
+    }
+    cout << -1;
+    return 0;
+}
