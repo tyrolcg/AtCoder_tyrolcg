504e071d86f4ef5d86ed3e47a53c4389fb9fc4ab
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-10-21 22:45:18 +0900

    A done

---
+++ AtCoder/ABC325/A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main(){
+    string s,t;
+    cin >> s;
+    cin >> t;
+    cout << s << " " << "san";
+    
+    return 0;
+}
