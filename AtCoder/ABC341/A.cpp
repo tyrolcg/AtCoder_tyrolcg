3b6dd2c358d7b917018d4e2d61de5c2ee9a02bd4
Tyrol Ema <tyrolcg.ema@gmail.com>
2024-02-17 22:37:11 +0900

    A done

---
+++ AtCoder/ABC341/A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        cout << "1";
+        cout << "0";
+    }
+    cout << "1" << endl;
+    
+    
+    return 0;
+}
